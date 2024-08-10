d410c0f3c670eb04558a59099a3f3b5d2192549d
minhquan24-source <quannguyen24082003@gmail.com>
2024-08-10 12:46:01 +1000

    Initial Commit

---
+++ VICHACK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VICHACK
+//
+//  Created by Quan Nguyen on 10/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VICHACK/VICHACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  VICHACKApp.swift
+//  VICHACK
+//
+//  Created by Quan Nguyen on 10/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct VICHACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
